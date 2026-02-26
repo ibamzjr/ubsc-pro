2e8d4baf93ffd929f75a3edfc21c7ce5913da397
ibamzjr <ilhamromadhonjr@gmail.com>
2026-02-27 06:30:14 +0700

    feat: add missing sections to HomePage component for complete layout

--- resources/js/Pages/HomePage.tsx
+++ resources/js/Pages/HomePage.tsx
@@ -3,6 +3,11 @@ import Navbar from "@/Components/Landing/Navbar";
 import SectionTwo from "@/Components/Landing/SectionTwo";
 import SectionThree from "@/Components/Landing/SectionThree";
 import SectionFour from "@/Components/Landing/SectionFour";
+import SectionFive from "@/Components/Landing/SectionFive";
+import SectionSix from "@/Components/Landing/SectionSix";
+import SectionSeven from "@/Components/Landing/SectionSeven";
+import SectionEight from "@/Components/Landing/SectionEight";
+import Footer from "@/Components/Landing/Footer";
 import { Head } from "@inertiajs/react";
 
 export default function HomePage() {
@@ -15,7 +20,12 @@ export default function HomePage() {
                 <SectionTwo />
                 <SectionThree />
                 <SectionFour />
+                <SectionFive />
+                <SectionSix />
+                <SectionSeven />
+                <SectionEight />
             </main>
+            <Footer />
         </>
     );
 }
