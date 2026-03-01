52d3428ab02640f78582cc981e413e1a19929c65
ibamzjr <ilhamromadhonjr@gmail.com>
2026-03-01 23:35:54 +0700

    feat: update dependencies and enhance Tailwind CSS configuration
    - Added new dependencies: @react-three/drei, @react-three/fiber, class-variance-authority, clsx, maath, maplibre-gl, tailwind-merge, tailwindcss-animate, and three.
    - Enabled dark mode in Tailwind CSS configuration.
    - Extended Tailwind CSS theme with new font families, colors, and border radius settings.
    - Included tailwindcss-animate plugin in Tailwind CSS configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: ["class"],
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
@@ -11,27 +12,84 @@ export default {
     ],
 
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ["BDO Grotesk", ...defaultTheme.fontFamily.sans],
-                monument: ['"Monument Extended"', "sans-serif"],
-                clash: ['"Clash Display"', "sans-serif"],
-                bdo: ['"BDO Grotesk"', "sans-serif"],
-                archivo: ["Archivo", "sans-serif"],
-            },
-            colors: {
-                navy: {
-                    800: "#0E2444",
-                    900: "#0B1E3B",
-                    950: "#071530",
-                },
-                "accent-red": "#D50000",
-            },
-            // maxWidth: {
-            //     "8xl": "88rem", // 1408px
-            // },
-        },
+    	extend: {
+    		fontFamily: {
+    			sans: [
+    				'BDO Grotesk',
+                    ...defaultTheme.fontFamily.sans
+                ],
+    			monument: [
+    				'Monument Extended"',
+    				'sans-serif'
+    			],
+    			clash: [
+    				'Clash Display"',
+    				'sans-serif'
+    			],
+    			bdo: [
+    				'BDO Grotesk"',
+    				'sans-serif'
+    			],
+    			archivo: [
+    				'Archivo',
+    				'sans-serif'
+    			]
+    		},
+    		colors: {
+    			navy: {
+    				'800': '#0E2444',
+    				'900': '#0B1E3B',
+    				'950': '#071530'
+    			},
+    			'accent-red': '#D50000',
+    			background: 'hsl(var(--background))',
+    			foreground: 'hsl(var(--foreground))',
+    			card: {
+    				DEFAULT: 'hsl(var(--card))',
+    				foreground: 'hsl(var(--card-foreground))'
+    			},
+    			popover: {
+    				DEFAULT: 'hsl(var(--popover))',
+    				foreground: 'hsl(var(--popover-foreground))'
+    			},
+    			primary: {
+    				DEFAULT: 'hsl(var(--primary))',
+    				foreground: 'hsl(var(--primary-foreground))'
+    			},
+    			secondary: {
+    				DEFAULT: 'hsl(var(--secondary))',
+    				foreground: 'hsl(var(--secondary-foreground))'
+    			},
+    			muted: {
+    				DEFAULT: 'hsl(var(--muted))',
+    				foreground: 'hsl(var(--muted-foreground))'
+    			},
+    			accent: {
+    				DEFAULT: 'hsl(var(--accent))',
+    				foreground: 'hsl(var(--accent-foreground))'
+    			},
+    			destructive: {
+    				DEFAULT: 'hsl(var(--destructive))',
+    				foreground: 'hsl(var(--destructive-foreground))'
+    			},
+    			border: 'hsl(var(--border))',
+    			input: 'hsl(var(--input))',
+    			ring: 'hsl(var(--ring))',
+    			chart: {
+    				'1': 'hsl(var(--chart-1))',
+    				'2': 'hsl(var(--chart-2))',
+    				'3': 'hsl(var(--chart-3))',
+    				'4': 'hsl(var(--chart-4))',
+    				'5': 'hsl(var(--chart-5))'
+    			}
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	}
     },
 
-    plugins: [forms],
+    plugins: [forms, require("tailwindcss-animate")],
 };
